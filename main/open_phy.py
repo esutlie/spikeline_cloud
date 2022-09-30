81509ddb36dca1612b2fd85a0513f37fd5947cb2
esutlie <elissasutlief@gmail.com>
2022-09-30 14:39:03 -0400

    edited folder path

--- main/open_phy.py
+++ main/open_phy.py
@@ -4,11 +4,11 @@ import os
 def open_phy(save_folder):
     anaconda_prompt_cmd = ' '.join([os.path.join('C:\\', 'Users', 'Elissa', 'Anaconda3', 'Scripts', 'activate.bat'),
                                     os.path.join('C:\\', 'Users', 'Elissa', 'Anaconda3')])
-    folder_path = f'cd /d D:\\SpikeInterface\\spike_interface\\{save_folder}'
+    folder_path = f'cd /d {save_folder}'
     os.system(
         r"""start "My Spyder Package Installer" /wait cmd /c "%s&%s&%s&%s" """ % (
             anaconda_prompt_cmd, 'conda activate phy2', folder_path, 'phy template-gui params.py'))
 
 
 if __name__ == '__main__':
-    open_phy('phy_folder_for_ironclust0')
+    open_phy('C:\github\spikeline\phy_folder_for_all_sort')
