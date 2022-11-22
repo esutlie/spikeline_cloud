2c308a47489b3829149e81c171c1da4d830918e1
esutlie <elissasutlief@gmail.com>
2022-11-22 13:43:25 -0500

    added cloud logging

--- apache/main.py
+++ apache/main.py
@@ -1,34 +1,71 @@
 import os
 import json
 import glob
+import argparse
 import traceback
-import subprocess
 import numpy as np
-import logging as log
 from sys import platform
 from shutil import rmtree
-from google.cloud import storage, logging
-
-logging_client = logging.Client(project='spikeline')
-logging_client.setup_logging()
-log.info('script started')
-
+from google.cloud import storage
 from sklearn.preprocessing import normalize
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io import ReadFromText
+
 import spikeinterface.full as si
 import spikeinterface.sorters as ss
 import spikeinterface.extractors as se
 import spikeinterface.comparison as sc
 from spikeinterface.exporters import export_to_phy
 
-log.info('spike interface imported')
+
+class RecordingExtraction(beam.DoFn):
+    """Read SpikeGLX data."""
+
+    def process(self, element):
+        return se.read_spikeglx(element, stream_id='imec0.ap')
 
 
-def cloud_sort(use_docker=False):
-    storage_client = storage.Client(project='spikeline')
-    if use_docker:
-        n_jobs = -1
+def run(argv=None, save_main_session=True):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default=os.path.join('D:\\', 'Test', 'ES029', 'ES029_2022-09-14_bot72_0_g0',
+                             'ES029_2022-09-14_bot72_0_g0_imec0', 'ES029_2022-09-14_bot72_0_g0_t0.imec0.ap.bin'),
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+    # The pipeline will be run on exiting the with block.
+    with beam.Pipeline(options=pipeline_options) as p:
+        # Read the text file[pattern] into a PCollection.
+        lines = p | 'Read' >> RecordingExtraction(known_args.input)
+
+        counts = (
+            lines
+            | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
+            | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+            | 'GroupAndSum' >> beam.CombinePerKey(sum))
+
+
+def apache_sort():
+    if platform == "linux" or platform == "linux2":
+        docker_image = True
     else:
-        n_jobs = 8
+        docker_image = None
+
+    storage_client = storage.Client(project='spikeline')
 
     json_path = os.path.join(os.getcwd(), 'sorting_history.json')
     input_bucket = storage_client.get_bucket('spikeline_input')
@@ -37,11 +74,9 @@ def cloud_sort(use_docker=False):
     input_bucket_folders = np.unique([b[0] for b in [os.path.normpath(blob.name).split(os.path.sep) for blob in
                                                      storage_client.list_blobs('spikeline_input')]])
 
-    log.info('Set up storage client and got buckets')
-    if 'sorting_history.json' in input_bucket_folders:
-        blob = input_bucket.blob('sorting_history.json')
-        blob.download_to_filename(json_path)
-        log.info('downloaded sorting_history')
+    # if 'sorting_history.json' in input_bucket_folders:
+    #     blob = input_bucket.blob('sorting_history.json')
+    #     blob.download_to_filename(json_path)
 
     if os.path.exists(json_path):
         with open('sorting_history.json', 'r') as openfile:
@@ -53,15 +88,12 @@ def cloud_sort(use_docker=False):
         json_object = json.dumps(sorting_history, indent=4)
         with open(json_path, "w") as outfile:
             outfile.write(json_object)
-            log.info('wrote new sorting_history')
 
     print(f'previously sorted: {sorting_history["sorting_complete"]}')
-    log.info(f'previously sorted: {sorting_history["sorting_complete"]}')
 
     for folder in input_bucket_folders:
         if folder != 'sorting_history.json' and folder not in sorting_history['sorting_complete']:
             print(f'{folder} not previously sorted. Beginning sort.')
-            log.info(f'{folder} not previously sorted. Beginning sort.')
             data_path = os.path.join(os.getcwd(), 'data')
             kilosort3_folder = os.path.join(os.getcwd(), 'kilosort3')
             kilosort2_5_folder = os.path.join(os.getcwd(), 'kilosort2_5')
@@ -75,37 +107,27 @@ def cloud_sort(use_docker=False):
                 if not os.path.exists(os.path.dirname(b_path)):
                     os.makedirs(os.path.dirname(b_path))
                 b.download_to_filename(b_path)
-            log.info(f'downloaded files from {folder}')
 
             recording_path = os.path.join(data_path, folder, folder + '_imec0')
-            log.info(f'specified recording save path: {recording_path}')
-
             recording = se.read_spikeglx(recording_path, stream_id='imec0.ap')
-            log.info(f'read spikeGLX')
 
             recording_cmr = recording
             recording_f = si.bandpass_filter(recording, freq_min=300, freq_max=6000)
             recording_cmr = si.common_reference(recording_f, reference='local', operator='median',
                                                 local_radius=(30, 200))
-            kwargs = {'n_jobs': n_jobs, 'total_memory': '8G'}
-            # kwargs = {'n_jobs': n_jobs}
-            log.info(f'applying filters...')
+            kwargs = {'n_jobs': 8, 'total_memory': '8G'}
             recording = recording_cmr.save(format='binary', folder=recording_save, **kwargs)
-            log.info(f'filters applied')
 
             sorter_params = {"keep_good_only": True}
             # ks3_sorter = si.read_sorter_folder(kilosort3_folder)
-            log.info(f'starting kilosort3...')
             ks3_sorter = ss.run_sorter(sorter_name='kilosort3', recording=recording, output_folder=kilosort3_folder,
-                                       verbose=False, docker_image=use_docker, **sorter_params)
+                                       verbose=False, docker_image=docker_image, **sorter_params)
             sorter_params = {"keep_good_only": False}
             # ks2_5_sorter = si.read_sorter_folder(kilosort2_5_folder)
-            log.info(f'starting kilosort2_5...')
             ks2_5_sorter = ss.run_sorter(sorter_name='kilosort2_5', recording=recording,
                                          output_folder=kilosort2_5_folder,
-                                         verbose=False, docker_image=use_docker, **sorter_params)
+                                         verbose=False, docker_image=docker_image, **sorter_params)
 
-            log.info(f'starting consensus...')
             consensus = sc.compare_multiple_sorters(sorting_list=[ks3_sorter, ks2_5_sorter],
                                                     name_list=['kilosort3', 'kilosort2_5'], verbose=False,
                                                     delta_time=.2,
@@ -122,21 +144,18 @@ def cloud_sort(use_docker=False):
                                   keepdims=True))) ** 2) for unit in consensus._properties['unit_ids']]) / 2
 
             consensus = consensus.select_units(consensus.unit_ids[np.where(template_similarty < .9)[0]])
-            log.info(f'trimmed with template matching')
 
+            if os.path.isdir(waveforms_folder):
+                rmtree(waveforms_folder)
             waveforms = si.WaveformExtractor.create(recording, consensus, waveforms_folder)
             waveforms.set_params(ms_before=3., ms_after=4., max_spikes_per_unit=500)
-            waveforms.run_extract_waveforms(n_jobs=n_jobs, chunk_size=30000)
+            waveforms.run_extract_waveforms(n_jobs=-1, chunk_size=30000)
             sparsity_dict = dict(method="radius", radius_um=50, peak_sign='both')
-            log.info(f'got waveforms')
-            log.info(f'starting phy export')
-            job_kwargs = {'n_jobs': n_jobs, 'total_memory': '8G'}
-            # job_kwargs = {'n_jobs': n_jobs}
+
+            job_kwargs = {'n_jobs': 8, 'total_memory': '8G'}
             export_to_phy(waveforms, phy_folder, compute_pc_features=True, compute_amplitudes=False, copy_binary=True,
                           remove_if_exists=True, sparsity_dict=sparsity_dict, max_channels_per_template=None,
                           **job_kwargs)
-            log.info(f'finished phy export')
-
             # try:
             #     export_to_phy(waveforms, phy_folder, compute_pc_features=True, compute_amplitudes=True,
             #                   copy_binary=True,
@@ -148,7 +167,9 @@ def cloud_sort(use_docker=False):
             #                   copy_binary=True,
             #                   remove_if_exists=True, sparsity_dict=sparsity_dict, max_channels_per_template=None,
             #                   **job_kwargs)
-            log.info(f'sending data to output folder')
+
+            np.save(os.path.join(phy_folder, 'template_similarty'), template_similarty)
+
             rel_paths = glob.glob(phy_folder + '/**', recursive=True)
             for local_file in rel_paths:
                 if os.path.isfile(local_file):
@@ -157,7 +178,6 @@ def cloud_sort(use_docker=False):
                     blob = output_bucket.blob(remote_path)
                     blob.upload_from_filename(local_file)
 
-            log.info(f'sending original data to archive folder')
             archive_bucket_blobs = [blob.name for blob in archive_bucket.list_blobs()]
             for b in input_bucket.list_blobs(prefix=folder):
                 if b.name not in archive_bucket_blobs:
@@ -170,18 +190,14 @@ def cloud_sort(use_docker=False):
                             src_blob, token=rewrite_token)
                         print(f'Progress so far: {bytes_rewritten}/{bytes_to_rewrite} bytes.')
 
-            log.info(f'editing sorting history.json')
             sorting_history['sorting_complete'].append(folder)
             sorting_history['sorting_complete'].sort()
-            log.info(f'writing sorting history.json')
             json_object = json.dumps(sorting_history, indent=4)
             with open(json_path, "w") as outfile:
                 outfile.write(json_object)
-            log.info(f'sending sorting history.json')
             blob = input_bucket.blob('sorting_history.json')
             blob.upload_from_filename(json_path)
 
-            log.info(f'removing data folders')
             rmtree(data_path)
             rmtree(kilosort3_folder)
             rmtree(kilosort2_5_folder)
@@ -189,66 +205,5 @@ def cloud_sort(use_docker=False):
             rmtree(recording_save)
 
 
-def delete():
-    name = os.popen(
-        'curl "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google"').read()
-    zone = os.popen(
-        'curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"').read()
-    log.info(f'name: {name}')
-    log.info(f'zone: {zone}')
-    # os.system(f'gcloud --quiet compute instances delete {name} --zone={zone}')
-    try:
-        result = os.system(f'gcloud compute instances delete instance-1 --zone=us-central1-a')
-        print(f'delete result: {result}')
-    except Exception as e1:
-        error_message = traceback.format_exc()
-        print(error_message)
-        log.error(error_message)
-        log.error(e1.args[0])
-
-
 if __name__ == '__main__':
-    if platform == "linux" or platform == "linux2":
-        linux = True
-    else:
-        linux = None
-
-    try:
-        log.info(f'starting cloud sort')
-        cloud_sort(use_docker=linux)
-        log.info(f'cloud sort finished')
-
-    except Exception as e:
-        log.info(f'ran into error:')
-        try:
-            error_message = traceback.format_exc()
-            print(error_message)
-            log.error(error_message)
-            log.error(e.args[0])
-        except Exception:
-            log.error('Tried to print error but couldn\'t.')
-    finally:
-        if linux:
-            delete()
-
-"""
-in terminal at containing folder i ran this:
-
-gcloud auth login
-
-then this:
-gcloud builds submit --pack image=gcr.io/spikeline/job-spikeline --project spikeline
-
-which builds the container.
-
-
-then i ran this: (all this next stuff doesnt work cause google run/jobs limits disk space to 32GiB
-
-gcloud beta run jobs create job-spikeline --image gcr.io/spikeline/job-spikeline --project spikeline --tasks 1 --max-retries 1 --region us-central1
-
-which creates the job.
-
-Next i ran this to run the job:
-
-gcloud beta run jobs execute job-spikeline --project spikeline --region us-central1
-"""
+    apache_sort()
--- main/open_phy.py
+++ main/open_phy.py
@@ -4,16 +4,17 @@ import numpy as np
 import json
 
 
-def download_next():
-    storage_client = storage.Client(project='spikeline')
+def curate_from_cloud(new=True):
     json_path = os.path.join(os.getcwd(), 'sorting_history.json')
-    output_bucket = storage_client.get_bucket('spikeline_output')
-    output_bucket_folders = np.unique([b[0] for b in [os.path.normpath(blob.name).split(os.path.sep) for blob in
-                                                      storage_client.list_blobs('spikeline_output')]])
+    if new:
+        storage_client = storage.Client(project='spikeline')
+        output_bucket = storage_client.get_bucket('spikeline_output')
+        output_bucket_folders = np.unique([b[0] for b in [os.path.normpath(blob.name).split(os.path.sep) for blob in
+                                                          storage_client.list_blobs('spikeline_output')]])
 
-    if 'sorting_history.json' in output_bucket_folders:
-        blob = output_bucket.blob('sorting_history.json')
-        blob.download_to_filename(json_path)
+        if 'sorting_history.json' in output_bucket_folders:
+            blob = output_bucket.blob('sorting_history.json')
+            blob.download_to_filename(json_path)
 
     if os.path.exists(json_path):
         with open('sorting_history.json', 'r') as openfile:
@@ -28,22 +29,34 @@ def download_next():
 
     print(f'previously curated: {sorting_history["curation_complete"]}')
     to_curate = None
-    for folder in output_bucket_folders:
-        if folder != 'sorting_history.json' and folder not in sorting_history['curation_complete']:
-            to_curate = folder
-            break
-    if to_curate is not None:
-        phy_folder = os.path.join(os.getcwd(), 'phy_export')
-        for b in output_bucket.list_blobs(prefix=to_curate):
-            b_path = os.path.normpath(b.name).split(os.path.sep)
-            b_path = os.path.join(phy_folder, *b_path)
-            if not os.path.exists(os.path.dirname(b_path)):
-                os.makedirs(os.path.dirname(b_path))
-            b.download_to_filename(b_path)
-        template_similarity = np.load(os.path.join(phy_folder, 'template_similarty'))
+    temp_folder = os.path.join(os.getcwd(), 'curation_temp')
+    if new:
+        for folder in output_bucket_folders:
+            if folder != 'sorting_history.json' and folder not in sorting_history['curation_complete']:
+                to_curate = folder
+                break
+        if to_curate is not None:
+            for b in output_bucket.list_blobs(prefix=to_curate):
+                b_path = os.path.normpath(b.name).split(os.path.sep)
+                b_path = os.path.join(temp_folder, *b_path)
+                if not os.path.exists(os.path.dirname(b_path)):
+                    os.makedirs(os.path.dirname(b_path))
+                b.download_to_filename(b_path)
+
+    if os.path.exists(temp_folder):
+        os.listdir(temp_folder)
+        phy_folder = os.path.join(temp_folder, os.listdir(temp_folder)[0], 'phy_export')
+        template_similarity = np.load(os.path.join(phy_folder, 'template_similarty.npy'))
         for i, val in enumerate(template_similarity):
             print(f'unit {i}: similarity {val}')
         open_phy(phy_folder)
+        response = input('finished curation? (y/n): ')
+        if response in ['y', 'Y', 'yes', 'Yes']:
+            print('saving results and deleting raw data file...')
+            # add code here to delete raw data locally, delete whole file on cloud and upload local to cloud
+        if response in ['n', 'N', 'no', 'No']:
+            print('leaving files as is. Run download curate_from_cloud(new=False) to continue later')
+            # do nothing I guess
 
 
 def open_phy(save_folder):
@@ -56,4 +69,5 @@ def open_phy(save_folder):
 
 
 if __name__ == '__main__':
-    open_phy('C:\github\spikeline\phy_folders\phy_folder_for_ES029_2022-09-15_bot96_1_g0')
+    curate_from_cloud(new=False)
+    # open_phy('C:\github\spikeline\phy_folders\phy_folder_for_ES029_2022-09-15_bot96_1_g0')
